eae3b9c29fb0cec4bc29c0a5895339ef8ee512cf
Aungpao <aungpao321@gmail.com>
2021-01-12 13:56:58 +0700

    v1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,25 @@
 //your code here!
+function tableCreate(){
+    var body = document.body,
+        tbl  = document.createElement('table');
+
+    for(var i = 0; i < inputRow; i++){
+        var tr = tbl.insertRow();
+        for(var j = 0; j < inputCol; j++){
+            if(i == 2 && j == 1){
+                break;
+            } else {
+                var td = tr.insertCell();
+                td.appendChild(document.createTextNode('Cell'));
+                td.style.border = '1px solid black';
+                if(i == 1 && j == 1){
+                    td.setAttribute('rowSpan', '2');
+                }
+            }
+        }
+    }
+    body.appendChild(tbl);
+}
+if(document.getElementById('button-create-table').clicked){
+    tableCreate()
+}
